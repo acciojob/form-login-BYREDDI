9033d628c56d99af812c8ded539c975803b9fb4a
BYREDDI RAMANA BABU <127107319+BYREDDI@users.noreply.github.com>
2023-09-19 13:30:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	let elements=document.getElementById("form1");
+	let firstName=elements[0];
+	let lastName=elements[1];
+	alert("firstName.value");
+	alert("lastName.value");
 
 }
